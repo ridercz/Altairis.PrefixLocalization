579bd589caa1d9f88f5882ca282189697389ef74
Michal Altair Valášek <github@rider.cz>
2024-03-06 18:52:29 +0100

    Use modern app startup model.

--- LocalizationSampleApp/Program.cs
+++ LocalizationSampleApp/Program.cs
@@ -1,22 +1,35 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Altairis.PrefixLocalization;
 
-namespace LocalizationSampleApp {
-    public class Program {
-        public static void Main(string[] args) {
-            CreateHostBuilder(args).Build().Run();
-        }
+/* Register services to the IoC/DI container *********************************/
+var builder = WebApplication.CreateBuilder(args);
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
+// Register Razor Pages
+builder.Services.AddRazorPages();
+
+// Register prefix localization library
+builder.Services.AddPrefixLocalization(options => {
+    // Define supported locales
+    options.LocaleMappings.Add(new PrefixToCultureMapping("cesky", "cs-CZ", "cs"));
+    options.LocaleMappings.Add(new PrefixToCultureMapping("english", "en-US", "en"));
+    options.LocaleMappings.Add(new PrefixToCultureMapping("deutsch", "de-DE", "de"));
+    // Add ignored paths
+    options.IgnorePaths.Add("^/content/.+");
+});
+
+/* Configure the application **********************************************/
+var app = builder.Build();
+
+// Always use prefix localization as a first middleware (with possible exception below)
+app.UsePrefixLocalization();
+
+// You may use static middleware as a first one, if you want all static files excluded from localization
+app.UseStaticFiles();
+
+// This is not exactly needed, but it helps to see error codes
+app.UseStatusCodePagesWithReExecute("/english/Errors/{0}");
+
+// Map razor pages
+app.MapRazorPages();
+
+/* Run the application ***************************************************/
+await app.RunAsync();
